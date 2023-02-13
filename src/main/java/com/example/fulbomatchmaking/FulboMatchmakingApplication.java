3815b3e4a19dafb6a7823fac9f4c94ba066971dc
janosiccardi <72583545+janosiccardi@users.noreply.github.com>
2023-02-13 16:56:20 -0300

    back created

---
+++ src/main/java/com/example/fulbomatchmaking/FulboMatchmakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fulbomatchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulboMatchmakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulboMatchmakingApplication.class, args);
+	}
+
+}
