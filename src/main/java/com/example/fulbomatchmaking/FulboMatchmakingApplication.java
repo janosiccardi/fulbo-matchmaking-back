6ee1c25e539d273e51647aff2ff9f3f0ce9ac5f8
fer9912 <72043284+fer9912@users.noreply.github.com>
2023-02-22 20:49:10 -0300

    Update FulboMatchmakingApplication.java

--- src/main/java/com/example/fulbomatchmaking/FulboMatchmakingApplication.java
+++ src/main/java/com/example/fulbomatchmaking/FulboMatchmakingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
+@EntityScan("com.example.fulbomatchmaking.business")
 public class FulboMatchmakingApplication {
 
 	public static void main(String[] args) {
